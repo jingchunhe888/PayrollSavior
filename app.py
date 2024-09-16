5a7b5817e127323543f6243ced590af87647ab5a
jingchunhe888 <132230624+jingchunhe888@users.noreply.github.com>
2024-09-16 12:47:08 -0400

    Update app.py
    User Guide Link

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def main():
     1. Checks whether Goldfine Timesheets has more employees than BLS Excel.  
     2. 'To Review' download folder for in-out-in-out formats.
     
-    **[User Guide](https://docs.google.com/document/d/1QMZrcSBi4Cax1r3_MLoRC-QLfkkZThgMhR2kpejT_5U/edit?usp=sharing)**''')
+    [View User Guide](https://docs.google.com/document/d/1QMZrcSBi4Cax1r3_MLoRC-QLfkkZThgMhR2kpejT_5U/edit?usp=sharing)''')
 
 
 
